f2845fb46a7cd3f3dd583a2460e8507f39a2be24
Grigoriy Baranov <baranov@w3m.co>
2020-02-08 23:39:26 +0300

    add methods: connection and disconnection

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use std::net::{Shutdown, TcpStream};
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -6,19 +9,44 @@ mod tests {
     }
 }
 
+#[allow(dead_code)]
 struct MeasurementTag {
     key: String,
     value: String,
 }
 
+#[allow(dead_code)]
 struct MeasurementField {
     key: String,
     value: String,
 }
 
+#[allow(dead_code)]
 struct Measurement {
     name: String,
     tags: Option<Vec<MeasurementTag>>,
     fields: Vec<MeasurementField>,
     timestamp: Option<i64>,
+}
+
+pub struct Influx {
+    uri: String,
+    stream: Option<TcpStream>,
+}
+
+impl Influx {
+    pub fn new(uri: String) -> Influx {
+        Influx { uri, stream: None }
+    }
+    pub fn connect(&mut self) -> Result<(), Box<dyn Error>> {
+        self.stream = Some(TcpStream::connect(&self.uri)?);
+        Ok(())
+    }
+    pub fn disconnect(&mut self) -> Result<(), Box<dyn Error>> {
+        match &self.stream {
+            Some(stream) => stream.shutdown(Shutdown::Both)?,
+            None => {}
+        }
+        Ok(())
+    }
 }

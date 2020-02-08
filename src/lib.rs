d2024da89a28f76e2b50b581e80712648a501475
Grigoriy Baranov <baranov@w3m.co>
2020-02-08 22:07:11 +0300

    add Measurement

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,20 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+struct MeasurementTag {
+    key: String,
+    value: String,
+}
+
+struct MeasurementField {
+    key: String,
+    value: String,
+}
+
+struct Measurement {
+    name: String,
+    tags: Option<Vec<MeasurementTag>>,
+    fields: Vec<MeasurementField>,
+    timestamp: Option<i64>,
+}

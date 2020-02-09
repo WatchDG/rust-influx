cbdf3b606441427319cd41337ccb25c066367c8b
Grigoriy Baranov <baranov@w3m.co>
2020-02-09 11:20:06 +0300

    add ping

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::fmt;
+use std::io::{Read, Write};
 use std::net::{Shutdown, TcpStream};
 
 #[cfg(test)]
@@ -29,6 +31,17 @@ struct Measurement {
     timestamp: Option<i64>,
 }
 
+#[derive(Debug)]
+struct InfluxError(String);
+
+impl fmt::Display for InfluxError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "Error: {}", self.0)
+    }
+}
+
+impl Error for InfluxError {}
+
 pub struct Influx {
     uri: String,
     stream: Option<TcpStream>,
@@ -39,7 +52,9 @@ impl Influx {
         Influx { uri, stream: None }
     }
     pub fn connect(&mut self) -> Result<(), Box<dyn Error>> {
-        self.stream = Some(TcpStream::connect(&self.uri)?);
+        let stream = TcpStream::connect(&self.uri)?;
+        stream.set_nodelay(true)?;
+        self.stream = Some(stream);
         Ok(())
     }
     pub fn disconnect(&mut self) -> Result<(), Box<dyn Error>> {
@@ -48,5 +63,18 @@ impl Influx {
             None => {}
         }
         Ok(())
+    }
+    pub fn ping(&mut self) -> Result<bool, Box<dyn Error>> {
+        match &mut self.stream {
+            Some(stream) => {
+                stream.write(
+                    format!("HEAD /ping HTTP/1.1\r\nHost: {}\r\n\r\n", self.uri).as_bytes(),
+                )?;
+                let mut buffer = [0u8; 12];
+                stream.read(&mut buffer)?;
+                Ok(buffer[9] == 0x32 && buffer[10] == 0x30 && buffer[11] == 0x34)
+            }
+            None => Err(Box::new(InfluxError("no stream".into()))),
+        }
     }
 }
